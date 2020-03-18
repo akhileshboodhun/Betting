44348ef46033c7716913fe4c76026d2340c67286
Yogeshm1699 <yogeshdass.mohall@umail.uom.ac.mu>
2020-03-18 11:00:54 +0400

    5th commit after copy pendrive

---
+++ frontend/admin/admin_cards.php
@@ -0,0 +1,45 @@
+<div class="col-sm-4">
+        <div class="card card-block">
+            <button>
+<div class="card-title">
+    <h1 class="display-3">Race</h1>
+</div>
+</button>
+    </div>
+</div>
+<div class="col-sm-4">
+        <div class="card card-block">
+            <button>
+<div class="card-title">
+    <h1 class="display-3">Horse</h1>
+</div>
+</button>
+    </div>
+</div>
+<div class="col-sm-4">
+        <div class="card card-block">
+            <button>
+<div class="card-title">
+    <h1 class="display-3">Jockey</h1>
+</div>
+</button>
+    </div>
+</div>
+<div class="col-sm-4">
+        <div class="card card-block">
+            <button>
+<div class="card-title">
+    <h1 class="display-3">User</h1>
+</div>
+</button>
+    </div>
+</div>
+<div class="col-sm-4">
+        <div class="card card-block">
+            <button>
+<div class="card-title">
+    <h1 class="display-3">Refund</h1>
+</div>
+</button>
+    </div>
+</div>
