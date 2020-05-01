9b007aace2533af6861397beae697e586f87eac4
Yogeshm1699 <yogeshdass.mohall@umail.uom.ac.mu>
2020-05-02 02:36:41 +0400

    modals and forms completed

--- frontend/admin/horse_form.php
+++ frontend/admin/horse_form.php
@@ -1,6 +1,41 @@
-<?php
-echo "lol";
-
-
-
-?>
+<div class="row">
+                    
+                    <div class="col-md-8 col-xs-12">
+                        <div class="white-box">
+                            <form class="form-horizontal form-material">
+                                <div class="form-group">
+                                    <label class="col-md-12">Horse Name</label>
+                                    <div class="col-md-12">
+                                        <input type="text" placeholder="Kishan" class="form-control form-control-line"> </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="example-email" class="col-md-12">Horse Date of Birth</label>
+                                    <div class="col-md-12">
+                                    <input type="date" placeholder="Date Of Birth" ></div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-md-12">Horse Weight</label>
+                                    <div class="col-md-12">
+                                    <input type="text" placeholder="500" class="form-control form-control-line"> </div>
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-sm-12">Stable </label>
+                                    <div class="col-sm-12">
+                                        <select class="form-control form-control-line">
+                                            <option>Stable 1</option>
+                                            <option>Stable 2</option>
+                                            <option>Stable 3</option>
+                                            <option>Stable 4</option>
+                                            <option>Stable 5</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="col-sm-12">
+                                        <button class="btn btn-success">Add Horse to database</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+ </div>
--- frontend/admin/race_form.php
+++ frontend/admin/race_form.php
@@ -3,41 +3,39 @@
                     <div class="col-md-8 col-xs-12">
                         <div class="white-box">
                             <form class="form-horizontal form-material">
-                            <div class="form-group">
-                                    <label class="col-md-12">Horse ID</label>
+                                <div class="form-group">
+                                    <label class="col-md-12">Race Name</label>
                                     <div class="col-md-12">
-                                        <input type="text" placeholder="Horse ID" class="form-control form-control-line"> </div>
+                                        <input type="text" placeholder="Silicon Valley" class="form-control form-control-line"> </div>
                                 </div>
                                 <div class="form-group">
-                                    <label class="col-md-12">Horse Name</label>
+                                    <label for="example-email" class="col-md-12">Date</label>
                                     <div class="col-md-12">
-                                        <input type="text" placeholder="Reeshal" class="form-control form-control-line"> </div>
+                                    <input type="date" placeholder="Date" ></div>
                                 </div>
                                 <div class="form-group">
-                                    <label for="example-email" class="col-md-12">Horse Date of Birth</label>
+                                    <label class="col-md-12">Distance (in Meters)</label>
                                     <div class="col-md-12">
-                                    <input type="date" placeholder="Date Of Birth" name="reg_user_dob"></div>
+                                    <input type="text" placeholder="1000" class="form-control form-control-line"> </div>
                                 </div>
                                 <div class="form-group">
-                                    <label class="col-md-12">Horse Weight</label>
-                                    <div class="col-md-12">
-                                    <input type="text" placeholder="500" class="form-control form-control-line"> </div>
-                                </div>
-                                <div class="form-group">
-                                    <label class="col-sm-12">Stable </label>
+                                    <label class="col-sm-12">No. of Horses </label>
                                     <div class="col-sm-12">
                                         <select class="form-control form-control-line">
-                                            <option>Stable 1</option>
-                                            <option>Stable 2</option>
-                                            <option>Stable 3</option>
-                                            <option>Stable 4</option>
-                                            <option>Stable 5</option>
+                                            <option> 1</option>
+                                            <option> 2</option>
+                                            <option> 3</option>
+                                            <option> 4</option>
+                                            <option> 5</option>
+                                            <option> 6</option>
+                                            <option> 7</option>
+                                            <option> 8</option>
                                         </select>
                                     </div>
                                 </div>
                                 <div class="form-group">
                                     <div class="col-sm-12">
-                                        <button class="btn btn-success">Add Horse to database</button>
+                                        <button class="btn btn-success">Add Race to database</button>
                                     </div>
                                 </div>
                             </form>
