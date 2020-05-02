76746b0399a73e3d246b45ae1b34894cd6a6ad2b
Akhilesh Boodhun <quantum19992011@gmail.com>
2020-05-02 14:51:39 +0400

    fixed add race

--- backend/admin/addrace.php
+++ backend/admin/addrace.php
@@ -2,10 +2,10 @@
 include('../../global/serverconnectionafterlogin.php');
 
     if ($stmt = $conn->prepare('INSERT INTO race (race_name, date_time, distance, no_horses) VALUES(:racename, :racedatetime, :dist, :numhorses)')){
-        $stmt->bindParam(':racename', $POST_['race_name']);
-        $stmt->bindParam(':racedatetime', $POST_['date_time']);
-        $stmt->bindParam(':dist', $POST_['distance']);
-        $stmt->bindParam(':numhorses', $POST_['no_horses']);
+        $stmt->bindParam(':racename', $_POST['race_name']);
+        $stmt->bindParam(':racedatetime', $_POST['date_time']);
+        $stmt->bindParam(':dist', $_POST['distance']);
+        $stmt->bindParam(':numhorses', $_POST['no_horses']);
         $stmt->execute();
         echo 'Success';
         //echo "<script>location.href = 'http://localhost/betting/frontend/admin/admindashboard.php';</script>";
