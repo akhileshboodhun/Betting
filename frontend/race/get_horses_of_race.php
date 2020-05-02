2cd89f2b62e5b61e7aaf2defb4c6f8072de6e75c
Yogeshm1699 <yogeshdass.mohall@umail.uom.ac.mu>
2020-05-02 20:09:55 +0400

    successfully added dynamic betting table

--- frontend/race/get_horses_of_race.php
+++ frontend/race/get_horses_of_race.php
@@ -4,26 +4,27 @@
                         <th scope="col">Horse No.</th>
                         <th scope="col">Horse</th>
                         <th scope="col">Jockey</th>
-                        <th scope="col">Odds</th>
                         <th scope="col">Stable</th>
+                        <th scope="col">Odds</th>
                         <th scope="col">Bet</th>
                         </tr>
                     </thead>
                     <tbody>
                     <?php 
 include ('../../global/serverconnectionafterlogin.php');
-     $stmt = $conn->prepare('SELECT h.horse_id, h.horse_name, h.horse_dob, h.horse_weight, s.stable_name
-                             FROM horse h LEFT JOIN stable s
-                             ON h.stable_id = s.stable_id
-                                                             ');
+     $stmt = $conn->prepare("SELECT h.horse_id, h.horse_name, h.horse_dob, s.stable_name, d.odds
+                             FROM horse h JOIN horse_race d JOIN race r JOIN stable s
+                             WHERE r.race_id = $raceid AND d.horse_id = h.horse_id AND h.stable_id = s.stable_id AND r.race_id = d.race_id
+                             
+                            ");
                                         $stmt->execute();
                                         while ($row_stmt = $stmt->fetch(PDO::FETCH_ASSOC)){
                                             echo "<tr>
                                                     <td>" . $row_stmt['horse_id'] . "</td>
                                                     <td>" . $row_stmt['horse_name'] . "</td>
                                                     <td>" . $row_stmt['horse_dob'] . "</td>
-                                                    <td>" . $row_stmt['horse_weight'] . "</td>
                                                     <td>" . $row_stmt['stable_name'] . "</td>
+                                                    <td>" . $row_stmt['odds'] . "</td>
                                                     <td><button class=\"btn\">BET</button> </td>
                                                     </tr>
                                                     ";
