5db2fb1c27f876ad9656a9f1119da73426995d18
Akhilesh Boodhun <quantum19992011@gmail.com>
2020-05-03 01:31:22 +0400

    add_race fixed

--- backend/admin/addrace.php
+++ backend/admin/addrace.php
@@ -1,16 +1,40 @@
 <?php 
 include('../../global/serverconnectionafterlogin.php');
-
+$select_horse_array = $_POST['select_horses'];
+$horse_array_size = count($select_horse_array);
     if ($stmt = $conn->prepare('INSERT INTO race (race_name, date_time, distance, no_horses) VALUES(:racename, :racedatetime, :dist, :numhorses)')){
         $stmt->bindParam(':racename', $_POST['race_name']);
         $stmt->bindParam(':racedatetime', $_POST['date_time']);
         $stmt->bindParam(':dist', $_POST['distance']);
-        $stmt->bindParam(':numhorses', $_POST['no_horses']);
+        $stmt->bindParam(':numhorses', $horse_array_size);
         $stmt->execute();
         echo 'Success';
        // echo "<script>location.href = 'http://localhost/betting/frontend/admin/admindashboard.php';</script>";
     }else{
         echo 'Could not prepare statement!';
     }
+
+    $list = $conn->prepare("SELECT race_id FROM race WHERE race_name = :racename ");
+    $list->bindParam(':racename',$_POST['race_name']);
+    $list->execute();
+    $rs= $list->fetch(PDO::FETCH_ASSOC);
+    $raceid = $rs['race_id'];
+    //echo $raceid;
+    
+
+
+    foreach($select_horse_array as $s_horse){
+        if ($stmt = $conn->prepare('INSERT INTO horse_race (race_id, horse_id) VALUES(:raceid, :horseid)')){
+            $stmt->bindParam(':raceid', $raceid);
+            $stmt->bindParam(':horseid', $s_horse);
+            $stmt->execute();
+            echo 'Success2';
+        // echo "<script>location.href = 'http://localhost/betting/frontend/admin/admindashboard.php';</script>";
+        }else{
+            echo 'Could not prepare statement!';
+        }
+    }
+
+
 
 ?>
