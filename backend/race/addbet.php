0bfb936b32125314aeb090f44036b2e4a0b55d4e
Akhilesh Boodhun <quantum19992011@gmail.com>
2020-05-04 01:06:56 +0400

    need to resolve post-addbet-processing

--- backend/race/addbet.php
+++ backend/race/addbet.php
@@ -1,9 +1,6 @@
 <?php 
 include('../../global/serverconnectionafterlogin.php');
-    
-    $usrid=155;
-    echo $usrid;
-    // Username doesnt exists, insert new account
+    // Insert New Bet
     if ($stmt = $conn->prepare("INSERT INTO bets (race_id, horse_id, userid, bet_odd, bet_amount) VALUES (:raceid, :horseid, :userid,:betodd,:betamount);")) {
         $stmt->bindParam(':raceid', $_POST['race_id']);
         $stmt->bindParam(':horseid', $_POST['horse_id']);
@@ -12,10 +9,47 @@ include('../../global/serverconnectionafterlogin.php');
         $stmt->bindParam(':betamount', $_POST['bet_amount']);
         $stmt->execute();
          echo 'Success';                      
-        //echo "<script>location.href = 'http://localhost/betting/frontend/admin/admindashboard.php';</script>";
     } else {
         // Something is wrong with the sql statement, check to make sure accounts table exists with all 3 fields.
         echo 'Could not prepare statement!';
     }
+
+    //update inconsistent table 'horse_race' for current horse
+$s_update = "   UPDATE horse_race
+                SET odds = :newodds
+                WHERE race_id = :raceid
+                AND   horse_id = :horseid
+
+;";
+
+if ($stmt = $conn->prepare($s_update)){
+    $stmt->bindParam(':raceid', $_POST['race_id']);
+    $stmt->bindParam(':horseid', $_POST['horse_id']);
+    $stmt->bindParam(':newodds', $_POST['bet_odd']);
+    $stmt->execute();
+    echo 'Success2';
+}else{
+    echo 'Could not prepare statement2!';
+}
+
+//update inconsistent table 'horse_race' for other horses
+$s_update = "   UPDATE horse_race
+                SET odds = odds + :new_other_odds_inc
+                WHERE race_id = :raceid
+                AND   horse_id <> :horseid
+
+;";
+
+if ($stmt = $conn->prepare($s_update)){
+    $stmt->bindParam(':raceid', $_POST['race_id']);
+    $stmt->bindParam(':horseid', $_POST['horse_id']);
+    $stmt->bindParam(':new_other_odds_inc', $_POST['other_odd_inc']);
+    $stmt->execute();
+    echo 'Success3';
+}else{
+    echo 'Could not prepare statement3!';
+}
+
+
 
 ?>
