c7e23004ea2899f6fdcb2baee8b2ebac94f33a4e
Yogeshm1699 <yogeshdass.mohall@umail.uom.ac.mu>
2020-05-04 18:54:10 +0400

    sdfghjkl;

---
+++ frontend/admin/select_horses2.php
@@ -0,0 +1,25 @@
+<?php include('../../global/serverconnectionafterlogin.php'); ?>
+</label>
+    <div class="col-sm-6">
+        <select name="select_horses2[]" class="resetfield form-control form-control-line">
+        <option value="">Select Horse</option>
+            <?php  
+                $list = $conn->prepare("select * from horse order by horse_id asc");
+                $list->execute();
+                while ($row_list = $list->fetch(PDO::FETCH_ASSOC))
+                    echo "<option value=\"" . $row_list['horse_id'] ."\">"  . $row_list['horse_name'] . "</option>" ;
+            ?>
+        </select>
+    </div>
+    <div class="col-sm-6">
+        <select name="select_jockeys[]"  class="resetfield form-control form-control-line">
+        <option value="">Select Jockey</option>
+            <?php  
+                $list = $conn->prepare("select * from jockey order by jockey_id asc");
+                $list->execute();
+                while ($row_list = $list->fetch(PDO::FETCH_ASSOC))
+                    echo "<option value=\"" . $row_list['jockey_id'] ."\">"  . $row_list['jockey_name'] . "</option>" ;
+            ?>
+        </select>
+    </div>
+    <a href="javascript:void(0);" class="remove_button fa fa-trash-o"></a></div>
