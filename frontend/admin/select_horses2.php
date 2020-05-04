9f7121fe1d64b88f41662984101a789753d9bcdb
Yogeshm1699 <yogeshdass.mohall@umail.uom.ac.mu>
2020-05-04 18:56:31 +0400

    slecthorses2[]

--- frontend/admin/select_horses2.php
+++ frontend/admin/select_horses2.php
@@ -1,25 +1,15 @@
 <?php include('../../global/serverconnectionafterlogin.php'); ?>
 </label>
-    <div class="col-sm-6">
-        <select name="select_horses2[]" class="resetfield form-control form-control-line">
-        <option value="">Select Horse</option>
-            <?php  
-                $list = $conn->prepare("select * from horse order by horse_id asc");
-                $list->execute();
-                while ($row_list = $list->fetch(PDO::FETCH_ASSOC))
-                    echo "<option value=\"" . $row_list['horse_id'] ."\">"  . $row_list['horse_name'] . "</option>" ;
-            ?>
-        </select>
-    </div>
-    <div class="col-sm-6">
-        <select name="select_jockeys[]"  class="resetfield form-control form-control-line">
-        <option value="">Select Jockey</option>
-            <?php  
-                $list = $conn->prepare("select * from jockey order by jockey_id asc");
-                $list->execute();
-                while ($row_list = $list->fetch(PDO::FETCH_ASSOC))
-                    echo "<option value=\"" . $row_list['jockey_id'] ."\">"  . $row_list['jockey_name'] . "</option>" ;
-            ?>
-        </select>
-    </div>
+<div class="col-sm-12">
+                                                        <select  name="select_horses2[]" class="resetfield form-control form-control-line">
+                                                                <option value="">participant</option>
+                                                                <?php  
+                                                                        $list = $conn->prepare("SELECT * FROM vw_create_result WHERE race_id = 796547");
+                                                                        $list->execute();
+                                                                        while ($row_list = $list->fetch(PDO::FETCH_ASSOC))
+                                                                        echo "<option value=\"" . $row_list['horse_id'] ."\">"  . $row_list['horse_name'] . "-" . $row_list['jockey_name'] . "</option>" ;
+                                                                ?>
+                                                        </select>
+                                                </div>
+    
     <a href="javascript:void(0);" class="remove_button fa fa-trash-o"></a></div>
